a0d7ad1cea34ae7f724f583ac4ac34ee795a274f
Dmytro621 <156955449+Dmytro621@users.noreply.github.com>
2024-11-03 19:07:32 +0100

    homework2

--- for.js
+++
@@ -1,9 +0,0 @@
-function test(text, letter) {
-    let test = text.split('')
-    return (test.filter(result => !letter.includes(result))
-    .join('')
-)
-}
-
-console.log(test('Как дела' , ['а', 'л']))
-
